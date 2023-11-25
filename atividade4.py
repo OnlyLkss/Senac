fd431930657a3617c3bf78294378b5f303a5f65a
OnlyLkss <145156986+OnlyLkss@users.noreply.github.com>
2023-11-25 00:52:54 -0300

    Add files via upload

---
+++ atividade4.py
@@ -0,0 +1,23 @@
+print("ax²+bx+c=0")
+
+a = float(input("Insira o valor de a: "))
+b = float(input("Insira o valor de b: "))
+c = float(input("Insira o valor de c: "))
+
+delta = b**2 - (4*a*c)
+
+if delta < 0: 
+    print(f'A equação {a}x² + {b}x + {c} = 0 não possui raízes reais.')
+    del(a, b, c, delta)
+
+else:
+    x1 = (-b + delta**0.5)/(2*a)
+    x2 = (-b - delta**0.5)/(2*a)
+
+    if delta > 0:
+        print(f'A equação {a}x² + {b}x + {c} = 0 possui 2 raízes reais: {x1} e {x2}')
+
+    elif delta == 0:
+        print(f'A equação {a}x² + {b}x + {c} = 0 possui apenas uma raíz real: {x1}')
+
+    del(a, b, c, delta, x1, x2)
